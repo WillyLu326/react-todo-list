bb532b31ad2ada3de544249db8b0dd70a6325e39
Willy Lu <luzh0326@gmail.com>
2020-06-30 14:51:39 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const TodoForm = ({ addTodoItem }: { addTodoItem: (text: string) => void }) => {
 
     addTodoItem(value);
     setValue('');
-    console.log('willy');
+    console.log('test');
   }
 
   return (
